682b3648f5cf5bbd1959d2c3ec575282169b8be7
Christopher Nimz <cmnfv@mac.com>
2017-08-10 11:31:01 -0400

    another try

--- main.js
+++ main.js
@@ -1,19 +1,65 @@
-const float = `
+function populate(id){
+const template2 = `<section>
+  <div class="object">
+    <img src="${customers.results[id].picture.large}">
+    <h1>${customers.results[id].name.first} ${customers.results[id].name.last}</h1>
+  </div>
+  <div class="content">
+  <p>${customers.results[id].location.city},
+            ${customers.results[id].location.state}
+           ${customers.results[id].location.postcode}</p>
+        <p>${customers.results[id].phone}</p>
+       <div class="ssn">
+     <p>${customers.results[id].id.value}</p></div>
+     </div>
+  </div>
+  </section>`;
+  document.querySelector(".directory").innerHTML = template2;
+  document.write(template2);
+  console.log(template2);
+}
+let i=0;
+while(i<customers.results.length){
+  // let name = document.querySelector(".object");
+    populate(i);
+    i++;
+  }
 
-  <div class="vitals">
-    <img src="${customers.results[0].picture.large}">
-    <h2>${customers.results[0].name.first} ${customers.results[0].name.last}</h2>
-    <h4>${customers.results[0].email}</h4>
-      <p>${customers.results[0].location.city},
-          ${customers.results[0].location.state}
-          ${customers.results[0].location.postcode}</p>
-          <p>${customers.results[0].phone}</p>
-          <div id="ssn">
-          <p>${customers.results[0].id.value}</p></div>
-        </div>
-  `;
 
-document.querySelector('.maleframe').innerHTML = float
+// function populate(id){
+// const float = `
+//
+// <section>
+//   <div class="vitals">
+//     <img src="${customers.results[id].picture.large}">
+//     <h2>${customers.results[id].name.first} ${customers.results[id].name.last}</h2>
+//     <h4>${customers.results[id].email}</h4>
+//       <p>${customers.results[id].location.city},
+//           ${customers.results[id].location.state}
+//           ${customers.results[id].location.postcode}</p>
+//           <p>${customers.results[id].phone}</p>
+//           <div id="ssn">
+//           <p>${customers.results[id].id.value}</p></div>
+//         </div>
+//         </section>
+//   `;
+//
+//       document.querySelector('.directory').innerHTML = float
+//         document.write(float);
+//           console.log(float);
+//
+// }
+// let i=0;
+// while(i<customers.results.length){
+//   // let name = document.querySelector('.vitals');
+//     populate(i);
+//     i++;
+//   }
+
+
+  // document.querySelector(".directory").innerHTML = template2;
+  // document.write(template2);
+  // console.log(template2);
 
 // photo ids are gender specific
 // function (){
